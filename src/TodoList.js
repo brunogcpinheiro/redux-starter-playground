46ceab3e3df1d2f58ad02ebe133723725d16c7fd
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-06-21 10:59:20 -0300

    Showing, Adding and Removing Todo's

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as TodoActions from "./store/actions/todos";
 
-const TodoList = (props) => (
-  <ul>
-    {props.todos.map(todo => <li>{todo.id} - {todo.text}</li>)}
-  </ul>
+const TodoList = ({ todos, addTodo, removeTodo }) => (
+  <Fragment>
+    {todos.map(todo => (
+      <li key={todo.id}>
+        {todo.id} - {todo.text}
+        <button onClick={() => removeTodo(todo.id)}>Remover</button>
+      </li>
+    ))}
+    <button onClick={() => addTodo("Novo Todo")}>Adicionar</button>
+  </Fragment>
 );
 
 const mapStateToProps = state => {
@@ -13,4 +21,7 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(TodoActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,11 +1,12 @@
-const INITIAL_STATE = [
-  { id: 1, text: "Fazer café" },
-  { id: 2, text: "Entrar na comunidade" },
-  { id: 3, text: "Estudar react" }
-];
+const INITIAL_STATE = [];
+let nextId = 0;
 
 export default function todos(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case "ADD_TODO":
+      return [...state, { id: nextId++, text: action.payload.text }];
+    case "REMOVE_TODO":
+      return state.filter(todo => todo.id !== action.payload.id);
     default:
       return state;
   }
